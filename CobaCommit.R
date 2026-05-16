f907fe1263cec8cc0dc94309ce3d3fed9b45ddd5
Daegan <daeganflabianos@gmail.com>
2026-05-16 16:26:34 +0700

    Visualisasi dan plot

--- CobaCommit.R
+++ CobaCommit.R
@@ -5,3 +5,12 @@ max(mtcars$mpg)
 min(mtcars)
 quantile(mtcars$mpg, 0,5)
 mtcars
+
+#Branch
+data(mtcars)
+head(mtcars)
+plot(mtcars$wt, mtcars$mpg,
+     main = "Hubungan berat mobil dan MPG",
+     xlab = 'berat mobil',
+     ylab = 'miles/galoon',
+     col = 'pink', pch = 19)
